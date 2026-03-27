d309cef9618f2e0590c9741771f343946d6ddd38
Shah Disha <149082713+ShahDishaK@users.noreply.github.com>
2026-03-27 17:42:19 +0530

    validate_user

--- helper/validation_helper.py
+++ helper/validation_helper.py
@@ -6,10 +6,8 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class ValidationHelper:
     def authenticate_user(username: str, password: str, db):
         user = db.query(User).filter(User.email == username).first()
-        print(user)
         if user is None:
             return False
-        print(bcrypt_context.verify(password, user.password))
         if not bcrypt_context.verify(password, user.password):
             return False
         return user
